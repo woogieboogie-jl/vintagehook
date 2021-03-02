891cd4e8338175116c87667ba48520a68ad62925
jaewooklee <woogieboogie.jl@gmail.com>
2021-03-03 04:23:37 +0900

    create overall crawling struture -1

---
+++ main.py
@@ -0,0 +1,7 @@
+import manager
+
+
+
+
+
+writeManager()
---
+++ manager.py
@@ -0,0 +1,12 @@
+import json
+
+shop_obj = open("vintageshop_list.json")
+shop_data = json.load(shop_obj)
+
+
+def writeManager(shop_data):
+
+
+
+
+def readManager(shop_data)
